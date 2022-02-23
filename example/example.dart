bba014bf6f6093f6476a110c96be4ee83c07a153
Kobi Lurie <kobi2187@gmail.com>
2022-02-23 11:18:53 +0200

    first commit

---
+++ example/example.dart
@@ -0,0 +1,26 @@
+// import 'package:test/test.dart';
+
+import '../lib/srt_parser.dart';
+
+const String data = '''1
+00:02:26,407 --> 00:02:31,356  X1:100 X2:100 Y1:100 Y2:100
+-- Arman I trust you, said the exhausted Adrian
+-- Arrrrrghhhhhh
+
+2
+00:02:31,567 --> 00:02:37,164 
+-- Lukas is publishing his library.
+-- I like the man.
+''';
+
+void main() {
+  List<Subtitle> subtitles = parseSrt(data);
+  for (Subtitle item in subtitles) {
+    print('ID is: ${item.id}');
+    print('Begin is: ${item.range?.begin} and End is: ${item.range?.end}');
+    for (String line in item.lines) {
+      print(line);
+    }
+    print('----');
+  }
+}
